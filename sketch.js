dba0ac3e7f6e92307cba9edad72b005f13d1159a
AgastyaIndla <63347849+AgastyaIndla@users.noreply.github.com>
2020-06-02 18:39:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var database;
+
+var drawing = [];
+var currentPath = [];
+
+function setup(){
+  canvas = createCanvas(1400,700);
+  canvas.mousePressed(startPath);
+  //canvas.mouseReleased(endPath);
+  database = firebase.database();
+ 
+}
+
+function startPath() {
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+function draw() {
+  background(0,100);
+  
+  if(mouseIsPressed) {
+    var point = {
+     x: mouseX,
+     y: mouseY 
+    }
+
+    currentPath.push(point);
+  }
+
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+
+  for (var i = 0; i < drawing.length; i++ ) {
+    var path = drawing[i];
+    beginShape();
+    for (var j = 0; j < path.length; j++) {
+    vertex(path[j].x, path[j].y );
+  }
+   endShape();
+  }
+
+  if(beginShape()){
+    drawing.update();
+  }
+
+  function update(){
+    var drawingIndex = "drawing"
+    database.ref(drawingIndex).set({ 
+    });
+  }
+
+}
